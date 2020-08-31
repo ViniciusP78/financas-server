39c57e29ea94b6dd7d54a8aae25eafbb5fb4d7e4
ViniciusP78 <vinicius0078@gmail.com>
2020-08-31 13:59:10 -0300

    Adicionado teste de autenticação

---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,9 @@
+import { Request, Response} from 'express';
+
+class UsersController {
+  index(request: Request, response: Response) {
+
+  }
+}
+
+export default UsersController;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+
+import routes from './routes'
 
 const app = express();
 
-app.get('/test', ( request, response ) => {
-  const filters = request.query;
-  console.log(filters);
-  response.send(`Hello ${filters}`);
-});
+app.use(cors());
+
+app.use(routes);
 
 app.listen(3333);
